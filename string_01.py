96162cfb90d65512406fd36ca74416f78c1e1b3e
Arvind Sharma <50014138+arsha15@users.noreply.github.com>
2025-06-13 09:43:46 +0200

    Add files via upload

---
+++ string_01.py
@@ -0,0 +1,4 @@
+name = "Sasha"
+color = 'Gold'
+print("Name is " + name + " and color is " + color)
+print(name*3)
---
+++ string_03.py
@@ -0,0 +1,5 @@
+pets="Cats & Dogs"
+pets.index("&")
+pets.index("C")
+pets.index("Dog")
+pets.index("s")
