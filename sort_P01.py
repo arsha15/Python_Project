013401177d5a86045eaeee7b47b5d9dcc84380d2
Arvind Sharma <50014138+arsha15@users.noreply.github.com>
2025-06-15 20:10:46 +0200

    Add files via upload

---
+++ sort_P01.py
@@ -0,0 +1,3 @@
+numbers = [4,8,1,54,2,6,9]
+numbers.sort()
+print(numbers)
---
+++ sort_P02.py
@@ -0,0 +1,9 @@
+Name_list = ["Arvind", "Shikha", "Yuvraj", "Ananya"]
+
+#Name_list.sort()
+
+print(Name_list)
+
+print(sorted(Name_list))
+
+print(Name_list)
