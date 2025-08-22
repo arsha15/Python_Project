7d6e0c5c346b4de4613f8fcbd43d540437da481b
Arvind Sharma <50014138+arsha15@users.noreply.github.com>
2025-08-22 12:51:35 +0200

    Update while_loop_01.py

--- while_loop_01.py
+++ while_loop_01.py
@@ -1,8 +1,9 @@
-def loop_fx(number):
+def loop_fx(number):# This is a comment
     x = number
     while x < 10 : 
         print("Hello iteration number " + str(x))
         x = x + 1
         print("now printing the " + str(x) + "th iteration")
+
 
 loop_fx(5)
