94e03c836947f7b1564e023cdab2850e427858d8
Arvind Sharma <50014138+arsha15@users.noreply.github.com>
2025-08-22 12:51:52 +0200

    Update foor_loop_03.py

--- foor_loop_03.py
+++ foor_loop_03.py
@@ -1,7 +1,8 @@
-def convert_to_celcius(given_temp):
+def convert_to_celcius(given_temp):# This is a comment
 #    return (x-32)*5/9
     final_result = (given_temp - 32) * (5/9)
     print(final_result)
 
 for i in range(1,3):
+
     convert_to_celcius(i)
