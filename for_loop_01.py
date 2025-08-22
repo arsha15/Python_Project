4c6998f6bc033854c5185e822f2fbd0b17a27989
Arvind Sharma <50014138+arsha15@users.noreply.github.com>
2025-08-22 12:52:07 +0200

    Update for_loop_01.py

--- for_loop_01.py
+++ for_loop_01.py
@@ -1,6 +1,7 @@
-for i in range(10):
+for i in range(10):# This is a comment
     print("Hi " + str(i))
 
 friends = ["Arvind", "Yuvraj", "Ananya", "Shikha"]
 for i in friends:
+
     print("Hello " + i)
