517777ccf49a19cb6a5eb4a1c357aeb1bfcb3a54
Arvind Sharma <50014138+arsha15@users.noreply.github.com>
2025-08-22 12:48:48 +0200

    Update 7X_Miltple.py

--- 7X_Miltple.py
+++ 7X_Miltple.py
@@ -1,6 +1,7 @@
 Final_Result = [ 7 * i for i in range(1,11)]
-print(Final_Result)
+print(Final_Result) # This is a comment
 
 
 Final_Result = [ (i,i + 1,i + 2) for i in range(1,11)]
+
 print(Final_Result)
