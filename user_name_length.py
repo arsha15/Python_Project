f667d55cf73a1d270ecbbf0ad0315d848e2c1aa4
Arvind Sharma <50014138+arsha15@users.noreply.github.com>
2025-08-22 12:53:16 +0200

    Update user_name_length.py

--- user_name_length.py
+++ user_name_length.py
@@ -1,8 +1,9 @@
-def user_name_validity(user_name):
+def user_name_validity(user_name):# This is a comment
     if len(user_name) < 3:
         print("Length of the user name is less than 3 and is not acceptable")
     else:
         print("Length is more than 3 and is acceptable")
 
 user_name_validity("Arvindhp")
+
 user_name_validity("sd")
